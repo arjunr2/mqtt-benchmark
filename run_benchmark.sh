d2099e5775b87d91923288c7110082667da07e5a
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-10-20 13:54:55 -0400

    Fixed script str uuidgen

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -3,20 +3,22 @@
 #hc cmd --devices hc-10 -x "cd mqtt-benchmark; ./benchmark --address=hc-00.arena.andrew.cmu.edu --name=\`uuidgen\` --interval=100000 --iterations=100 --topic=\`uuidgen\` --drop-ratio=10"
 
 ADDRESS=hc-00.arena.andrew.cmu.edu
-NAME=$(uuidgen)
-TOPIC=$(uuidgen)
+NAME=\`uuidgen\`
+TOPIC=\`uuidgen\`
 INTERVAL=100000
 ITER=100
 DROP_RATIO=10
 
 OUT=""
 
+script_str="cd mqtt-benchmark; ./benchmark --address=$ADDRESS --name=$NAME --interval=$INTERVAL --iterations=$ITER --topic=$TOPIC --drop-ratio=$DROP_RATIO"
+
 case $1 in
 	nointerference)
-		OUT=$(hc cmd -x "cd mqtt-benchmark; ./benchmark --address=$(ADDRESS) --name=$(NAME) --interval=$(INTERVAL) --iterations=$(ITER) --topic=$(TOPIC) --drop-ratio=$(DROP_RATIO)")
+		OUT=$(hc cmd -x "$script_str")
 		;;
 	isolated)
-		OUT=$(hc cmd --sync -x "cd mqtt-benchmark; ./benchmark --address=$(ADDRESS) --name=$(NAME) --interval=$(INTERVAL) --iterations=$(ITER) --topic=$(TOPIC) --drop-ratio=$(DROP_RATIO)")
+		OUT=$(hc cmd --sync -x "$script_str")
 		;;
 	*)
 		echo "Invalid argument (options: nointerference | isolated)"
