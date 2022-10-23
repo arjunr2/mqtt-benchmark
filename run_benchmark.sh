e1d5934608748bfc9b1f8c244edd4d1f22c3b0ae
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-10-23 18:42:40 -0400

    Added bash command args

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#hc cmd --devices hc-10 -x "cd mqtt-benchmark; ./benchmark --address=hc-00.arena.andrew.cmu.edu --name=\`uuidgen\` --interval=100000 --iterations=100 --topic=\`uuidgen\` --drop-ratio=10"
+shopt -s expand_aliases
+source ~/.alias
 
 ADDRESS=hc-00.arena.andrew.cmu.edu
 NAME=\`uuidgen\`
@@ -9,25 +10,43 @@ INTERVAL=100000
 ITER=100
 DROP_RATIO=10
 QOS=0
+LOG=""
+SIZE=64
 
 OUT=""
 
-script_str="cd mqtt-benchmark; ./benchmark --address=$ADDRESS --name=$NAME --interval=$INTERVAL --iterations=$ITER --topic=$TOPIC --drop-ratio=$DROP_RATIO --qos=$QOS"
+while getopts 'a:n:m:i:q:s:d:t:v' opt
+do
+	case "${opt}" in
+		a) ADDRESS=$OPTARG;;
+		n) NAME=$OPTARG;;
+		m) INTERVAL=$OPTARG;;
+		i) ITER=$OPTARG;;
+		q) QOS=$OPTARG;;
+		s) SIZE=$OPTARG;;
+		d) DROP_RATIO=$OPTARG;;
+		t) TOPIC=$OPTARG;;
+		v) LOG="--log";;
+	esac
+done
+BENCH_TYPE=${@:$OPTIND:1}
 
-case $1 in
+script_str="cd mqtt-benchmark; ./benchmark --address=$ADDRESS --name=$NAME --interval=$INTERVAL --iterations=$ITER --topic=$TOPIC --drop-ratio=$DROP_RATIO --qos=$QOS --size=$SIZE $LOG"
+echo $script_str
+
+
+case $BENCH_TYPE in
 	nointerference)
-		OUT=$(hc cmd -x "$script_str")
-		;;
+		OUT=$(hc cmd -x "$script_str") ;;
 	isolated)
-		OUT=$(hc cmd --sync -x "$script_str")
-		;;
+		OUT=$(hc cmd --sync -x "$script_str") ;;
 	*)
 		echo "Invalid argument (options: nointerference | isolated)"
 		exit 1
 		;;
 esac
 
-echo "$1 ; Interval=$INTERVAL ; Iter=$ITER ; QOS=$QOS ; Drop=$DROP_RATIO" > $1.results  
+echo "$1 | Interval=$INTERVAL ; Iter=$ITER ; QOS=$QOS ; Drop=$DROP_RATIO" > $1.results  
 echo "$OUT" | tee log \
 	  | awk '/\[hc-[0-9]+\]/ {print $1 nr[NR+15] nr[NR+16];next}; NR in nr' \
 	  | sed "s/.*/&,/" \
