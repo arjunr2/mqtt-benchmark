d5a82362ae513baad68dc51282001cee1dfa1a6c
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-10-23 19:29:07 -0400

    Added options for start/stop bench

--- Makefile
+++ Makefile
@@ -3,5 +3,11 @@
 all:
 	gcc mqtt.c -lpthread -lpaho-mqtt3cs -lm -o benchmark
 
+start-benchmark:
+	screen -S mqtt-benchmark -dm bash -c "./run_benchmark_set.sh"
+
+stop-benchmark:
+	screen -S mqtt-benchmark -p 0 -X stuff "^C"
+
 clean:
 	rm benchmark
--- run_benchmark_set.sh
+++ run_benchmark_set.sh
@@ -5,8 +5,8 @@ RESULT_DIR=results
 rm -r $RESULT_DIR
 mkdir -p $RESULT_DIR
 
-intervals=(2000 10000 30000 100000)
-sizes=(64 128 256 512)
+intervals=(2000 5000 10000 20000 40000 100000)
+sizes=(64 128 256 512 1024 4096 65536)
 for interval in "${intervals[@]}"
 do
 	for size in "${sizes[@]}"
