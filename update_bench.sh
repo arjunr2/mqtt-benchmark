ef4fcb58c2bdd2a6456cdb7c1d9e58e3c6d93e0a
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-12-22 18:40:43 -0500

    update benhc clone if doesn't exist

--- update_bench.sh
+++ update_bench.sh
@@ -3,4 +3,4 @@
 shopt -s expand_aliases
 source ~/.alias
 
-hc cmd -x "cd mqtt-benchmark; git pull; make"
+hc cmd -x "[ ! -d \"mqtt-benchmark\" ] && git clone https://github.com/arjunr2/mqtt-benchmark.git; cd mqtt-benchmark; git pull; make"
