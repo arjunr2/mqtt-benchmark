23b45b149d6abc1b13529d62098ef00dcf99b05f
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-12-30 23:12:49 -0500

    Added gptp log failure check

--- gptp/gptp_log.sh
+++ gptp/gptp_log.sh
@@ -5,9 +5,11 @@ source ~/.alias
 source gptp_env.sh
 
 echo "--- PTP LOGS ---"
+hc cmd --devices $GM_NODE $FW_NODES --action sudo --password wiselab2022 -x "pgrep ptp4l" 
 hc cmd --devices $GM_NODE $FW_NODES --action sudo --password wiselab2022 -x "tail -n6 $GPTP_DIR/ptp.log" 
-echo "----------------\n"
+echo -e "----------------\n"
 echo "--- PHC2SYS LOGS ---"
+hc cmd --devices $GM_NODE $FW_NODES --action sudo --password wiselab2022 -x "pgrep phc2sys" 
 hc cmd --devices $GM_NODE $FW_NODES --action sudo --password wiselab2022 -x "tail -n6 ./$GPTP_DIR/phc2sys.log" 
-echo "--------------------\n"
+echo -e "--------------------\n"
 
